71451a1257fd39109979bab78226a9c260408154
naylaaurellia18-web <naylaaurellia18@student.uns.ac.id>
2026-05-26 12:44:07 +0700

    config

--- api/config.php
+++ api/config.php
@@ -1,16 +1,26 @@
 <?php
 // Konfigurasi koneksi ke TiDB Cloud
 $host     = 'gateway01.ap-northeast-1.prod.aws.tidbcloud.com'; 
-$port     = 4000; // TiDB menggunakan port 4000, bukan 3306
+$port     = 4000; 
 $user     = '3vTUmEehdVYc5pg.root';
 $password = 'Pd8EOwUWoHfM5feG';
 $database = 'senjatrack-db';
 
-// Membuat koneksi ke server database
-$koneksi = mysqli_connect($host, $user, $password, $database, $port);
+// Inisialisasi MySQLi
+$koneksi = mysqli_init();
+
+if (!$koneksi) {
+    die("Inisialisasi MySQLi gagal");
+}
+
+// Mengaktifkan SSL tanpa harus menunjuk ke file .crt spesifik di OS
+mysqli_options($koneksi, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+// Lakukan koneksi menggunakan flag SSL
+$real_connect = mysqli_real_connect($koneksi, $host, $user, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
 
 // Cek apakah koneksi berhasil atau gagal
-if (!$koneksi) {
+if (!$real_connect) {
     die("Koneksi ke database Senja Track gagal: " . mysqli_connect_error());
 }
 ?>
